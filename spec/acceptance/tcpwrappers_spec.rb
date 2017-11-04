40367c7ea0b3d536500000d777c90876d07675cd
Jeffrey Miller <millerjl1701@gmail.com>
2017-11-03 19:10:17 -0500

    acceptance test switch to a let() for puppet code

--- spec/acceptance/tcpwrappers_spec.rb
+++ spec/acceptance/tcpwrappers_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper_acceptance'
 
 describe 'tcpwrappers class' do
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'should work idempotently with no errors' do
-      pp = <<-EOS
-      class { 'tcpwrappers': }
-      EOS
+  context 'with default parameters' do
+    let(:pp) { "class { 'tcpwrappers': }" }
 
+    it 'should work idempotently with no errors' do
       # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes  => true)
