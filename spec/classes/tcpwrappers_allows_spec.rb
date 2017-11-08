fc3c2a5eb8affb631700ce2f99ab0b5bfe78ca22
Jeffrey Miller <millerjl1701@gmail.com>
2017-11-08 11:26:08 -0600

    Added tiered hiera data files to test merge behavior

--- spec/classes/tcpwrappers_allows_spec.rb
+++ spec/classes/tcpwrappers_allows_spec.rb
@@ -14,11 +14,17 @@ describe 'tcpwrappers::allows', :type => :class do
 
       it { is_expected.to contain_class('tcpwrappers') }
       it { is_expected.to contain_tcpwrappers__allow('spec_telnet') }
+      it { is_expected.to contain_tcpwrappers__allow('spec_httpd') }
       it { is_expected.to contain_tcpwrappers__allow('spec_vsftpd') }
 
       it { is_expected.to contain_concat__fragment('tcpwrappers_spec_telnet').with(
               'target' => '/etc/hosts.allow',
               'order' => '22_telnet_all',
+      ) }
+
+      it { is_expected.to contain_concat__fragment('tcpwrappers_spec_httpd').with(
+              'target' => '/etc/hosts.allow',
+              'order' => '80_httpd_all',
       ) }
   
       it { is_expected.to contain_concat__fragment('tcpwrappers_spec_vsftpd').with(
